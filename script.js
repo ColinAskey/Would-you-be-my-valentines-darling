adb4cdd8b408a8362782ee7895fc609bcc9aaf3e
ColinAskey <danespinosa10401@gmail.com>
2025-02-12 21:21:01 -0600

    Update script.js

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function Sí() {
 
 
 var phrase = ['¿Cómo que NO?', '¿De verdad NO quieres?(ভ_ ভ)', '¿PORQUÉ NOOO?ರ╭╮ರ', 'YAAAAA!(╥_╥) NO juegues conmigo ಠ_ಠ)', 'Te daré una última oportunidad :))', 'Shi(„ᵕᴗᵕ„)♡ NO hay de otra']
-var gifs = ['/assets/como.jpg', '/assets/verdad.jpg', '/assets/esquina.jpg', '/assets/yaaaa.jpg', '/assets/indignado.jpg', '/assets/shi.jpg']
+var gifs = ['Would-you-be-my-valentines-darling/assets/como.jpg', 'Would-you-be-my-valentines-darling/assets/verdad.jpg', 'Would-you-be-my-valentines-darling/assets/esquina.jpg', 'Would-you-be-my-valentines-darling/assets/yaaaa.jpg', 'Would-you-be-my-valentines-darling/assets/indignado.jpg', 'Would-you-be-my-valentines-darling/assets/shi.jpg']
 var noCount = 0
 
 function no() {
