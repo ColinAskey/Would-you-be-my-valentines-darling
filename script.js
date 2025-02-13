646d86681f8407ed6d0a2b51226873dfb7b3a40c
ColinAskey <danespinosa10401@gmail.com>
2025-02-12 23:06:17 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,25 @@
 const gif = document.querySelector('.giffy');
-const yesBtn = document.querySelector('.yes');
+const yesBtn = document.querySelector('.Sí');
 const noBtn = document.querySelector('.no');
 const question = document.querySelector('.question')
 const buttons = document.querySelector('.buttons')
 const text = document.querySelector('.text')
 const body = document.querySelector('body')
 
-
-
-function yes() {
+function Sí() {
     text.style.display = 'block'
     buttons.style.display = 'none'
-    question.textContent = "Yey! It's a date then"
+    question.textContent = "Ahuevo! Es una cita nena <3"
     gif.src = 'assets/flores.jpg'
-
-
     createHearts(100);
 }
 
-
-
-
-var phrase = ['¿Cómo que NO?', '¿De verdad NO quieres?(ভ_ ভ)', '¿PORQUÉ NOOO?ರ╭╮ರ', 'YAAAAA!(╥_╥) NO juegues conmigo ಠ_ಠ)', 'Te daré una última oportunidad :))', 'Shi(„ᵕᴗᵕ„)♡ NO hay de otra'];
-var gifs = ['assets/como.jpg', 'assets/verdad.jpg', 'assets/esquina.jpg', 'assets/yaaaa.jpg', 'assets/indignado.jpg', 'assets/shi.jpg'];
-var noCount = 0;
+var phrase = ['¿Cómo que NO?', '¿De verdad NO quieres?(ভ_ ভ)', '¿PORQUÉ NOOO?ರ╭╮ರ', 'YAAAAA!(╥_╥) NO juegues conmigo ಠ_ಠ)', 'Te daré una última oportunidad :))', 'Shi(„ᵕᴗᵕ„)♡ NO hay de otra']
+var gifs = ['assets/como.jpg', 'assets/verdad.jpg', 'assets/esquina.jpg', 'assets/yaaaa.jpg', 'assets/indignado.jpg', 'assets/shi.jpg']
+var noCount = 0
 
 function no() {
-    if (noCount != 5) {
+    if (noCount != 6) {
         noBtn.style.position  = 'absolute'
         var newX = Math.random() * (window.innerWidth - noBtn.offsetWidth);
         var newY = Math.random() * (window.innerHeight - noBtn.offsetHeight);
@@ -35,33 +28,27 @@ function no() {
         noBtn.textContent = phrase[noCount];
         gif.src = gifs[noCount]
         noCount++;
-    }else {
-
-        yes()
+    } else {
+        Sí()
     }
-
-
 }
-
-
 
 function createHearts(numbers) {
-
-for (var i = 0; i < numbers; i++) {
-    var heart = document.createElement("img");
-
-    heart.src = '/assets/Heart_corazón.svg.png';
-    heart.alt = "Heart";
-    heart.classList.add("heart");
-    heart.classList.add("heart" + (i % 3 + 1)); // Assigning different animation classes
-    var size = Math.floor(Math.random() * 51) + 50; // Random size between 50 and 100
-    heart.style.width = size + "px";
-    heart.style.height = size + "px";
-    heart.style.left = Math.random() * (window.innerWidth - size) + "px";
-    heart.style.top = Math.random() * (window.innerHeight - size) + "px";
-    document.querySelector('body').appendChild(heart);
-    setTimeout(function() {
-        heart.remove();
-    }, 1500); // Remove the heart after 1.5 seconds
+    for (var i = 0; i < numbers; i++) {
+        var heart = document.createElement("img");
+        heart.src = 'assets/Heart_corazón.svg.png';
+        heart.alt = "Heart";
+        heart.classList.add("heart");
+        heart.classList.add("heart" + (i % 3 + 1)); // Assigning different animation classes
+        var size = Math.floor(Math.random() * 51) + 50; // Random size between 50 and 100
+        heart.style.width = size + "px";
+        heart.style.height = size + "px";
+        heart.style.left = Math.random() * (window.innerWidth - size) + "px";
+        heart.style.top = Math.random() * (window.innerHeight - size) + "px";
+        document.querySelector('body').appendChild(heart);
+        setTimeout(function() {
+            heart.remove();
+        }, 1500); // Remove the heart after 1.5 seconds
+    }
 }
-}
+
